cba021f4651c3647a43e2c3774af2eccd26ff09d
stark501 <mkudlacej@gmail.com>
2021-11-19 15:40:04 +0100

    Zmena textu

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -6,7 +6,7 @@ namespace Open_Lab_10._00
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ahoj svet !");
+            Console.WriteLine("Hello World !!!");
         }
     }
 }
